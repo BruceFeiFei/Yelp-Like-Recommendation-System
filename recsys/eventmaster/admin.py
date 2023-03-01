b28d31c679de2ed40fd6c8f458a8d23a7b9b91db
BruceFeiFei <2064129551@qq.com>
2023-03-01 00:05:45 -0500

    Change sqlite to MySql
    Add model Rating; Change database to MySql

--- recsys/eventmaster/admin.py
+++ recsys/eventmaster/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Event
+from .models import User, Event, Rating
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Event)
+admin.site.register(Rating)
--- recsys/eventmaster/models.py
+++ recsys/eventmaster/models.py
@@ -9,5 +9,18 @@ class User(models.Model):
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
-    users = models.ManyToManyField(to=User, blank=True)
+    users = models.ManyToManyField(to=User, blank=True, through='Rating')
+
+
+class Rating(models.Model):
+    RATING_STARS = [
+        (1, 'One Star'),
+        (2, 'Two Star'),
+        (3, 'Three Star'),
+        (4, 'Four Star'),
+        (5, 'Five Star'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    star = models.IntegerField(choices=RATING_STARS)
 
